e2783db85e675fbddaec8a1db2acf2674e61e3f3
DemetriusBS <dbsfox@outlook.com>
2021-01-18 10:19:32 -0300

    Github App - Criando a marcação da aplicação

--- src/app.js
+++ src/app.js
@@ -2,9 +2,45 @@
 
 import React from 'react'
 
-const App = React.createClass({
-  render: function () {
-    return <h1>Aplicação 1</h1>
-  }
-})
+const App = () => (
+  <div className='app'>
+    <div className='search'>
+      <input type='search' placeholder='Digite o nome do usuário no Github' />
+    </div>
+
+    <div className='user-info'>
+      <img src='https://avatars0.githubusercontent.com/u/59734496?v=4' />
+      <h1>
+        <a href='github.com/DemetriusBS'>Demetrius Batista dos Santos</a>
+      </h1>
+
+      <ul className='repos-info'>
+        <li>Repositórios: 20</li>
+        <li>Seguidores: 10</li>
+        <li>Seguindo: 10</li>
+      </ul>
+    </div>
+
+    <div className='actions'>
+      <button>Ver repositórios</button>
+      <button>Ver favoritos</button>
+    </div>
+
+    <div className='repos'>
+      <h2>Repositórios:</h2>
+      <ul>
+        <li><a href='#'>Nome do repositório</a></li>
+      </ul>
+    </div>
+
+    <div className='starred'>
+      <h2>Favoritos:</h2>
+      <ul>
+        <li><a href='#'>Nome do repositório</a></li>
+      </ul>
+    </div>
+
+  </div>
+)
+
 export default App
